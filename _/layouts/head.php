73a797e23151b14e82f8e1cf7331284d5bee9db3
junpei-8 <junpei.v8@gmail.com>
2024-09-06 17:11:10 +0900

    update: head タグでタイトルと詳細を props から変更できるように修正

--- _/layouts/head.php
+++ _/layouts/head.php
@@ -1,7 +1,21 @@
+<?php
+// タイトルの設定
+$default_title = isset($props['default_title']) ? $props['default_title'] : 'Static Site PHP Base';;
+$raw_title = isset($props['title']) ? $props['title'] : null;
+$title_template = isset($props['title_template']) ? $props['title_template'] : "%s | $default_title";
+$title = $raw_title ? sprintf($title_template, $raw_title) : $default_title;
+
+// 説明文の設定
+$description = isset($props['description']) ? $props['description'] : null;
+?>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Static Site PHP Base</title>
+<title><?php echo $title ?></title>
+<?php if ($description): ?>
+    <meta name="description" content="<?php echo $description ?>">
+<?php endif; ?>
 
 <link rel="stylesheet" href="/_/styles/global.min.css">
 
