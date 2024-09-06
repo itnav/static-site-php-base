7848b52bd356d7b1b32283392b5673f85398b2a8
junpei-8 <junpei.v8@gmail.com>
2024-09-06 17:11:49 +0900

    update: ボタンの CLS を軽減

--- _/components/button/button.php
+++ _/components/button/button.php
@@ -3,4 +3,6 @@ teleport_style('head', path_resolve(__DIR__, './button.min.css'));
 teleport_script('head', path_resolve(__DIR__, './button.js'), array('defer' => ''));
 ?>
 
-<button class="button" data-label="<?php echo $args['label'] ?>"></button>
+<button class="button" data-label="<?php echo $props['label'] ?>">
+    &nbsp;
+</button>
